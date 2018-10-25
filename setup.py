c641fcc31eb14eac535a20db577c8407338f8154
pkos98 <patrick.kostas@hs-augsburg.de>
2018-10-25 17:21:33 +0200

    Applied basic project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='cassiopeia',
+   version='0.1.0',
+   description='Course project for prog3',
+   author='Patrick Kostas, Hanne Beuter, Dejan Pekez, Benjamin Altmiks, Lukas Kleybolte',
+   author_email='patrick.kostas@hs-augsburg.de, hanne.beuter@hs-augsburg.de, dejan.pekez@hs-augsburg.de,
+   benjamin.altimiks@hs-augsburg.de, lukas.kleybolte@hs-augsburg.de',
+   packages=['cassiopeia']
+)
