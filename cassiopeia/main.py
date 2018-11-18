f1218ac3a7189d8494d5bf45293cf5b4fbd58650
pkos98 <patrick.kostas@hs-augsburg.de>
2018-11-18 22:30:40 +0100

    Implement debug-flags & exit using SIGINT

--- cassiopeia/main.py
+++ cassiopeia/main.py
@@ -21,25 +21,39 @@ Start simulation and renderer in separate processes connected by a pipe.
 #
 import multiprocessing
 import time
-
 from rendering import simulation_bridge, galaxy_renderer
 from rendering.simulation_constants import END_MESSAGE
+from signal import SIGINT, signal
+from sys import argv, exit
 
 def _startup():
     renderer_conn, simulation_conn = multiprocessing.Pipe()
+    physics_debug_mode, render_debug_mode = False, False
+    if len(argv) > 1:
+        if argv[1] == "--debug-physics": physics_debug_mode = True
+        elif argv[1] == "--debug-rendering": render_debug_mode = True
     simulation_process = \
         multiprocessing.Process(target=simulation_bridge.startup,
-                                args=(simulation_conn, 16, 1))
+                                args=(simulation_conn, 16, 1, physics_debug_mode))
     render_process = \
         multiprocessing.Process(target=galaxy_renderer.startup,
-                                args=(renderer_conn, 60), )
+                                args=(renderer_conn, 60, render_debug_mode))
     simulation_process.start()
     render_process.start()
-    time.sleep(10)
-    simulation_conn.send(END_MESSAGE)
-    renderer_conn.send(END_MESSAGE)
-    simulation_process.join()
-    render_process.join()
+
+    # Gets called on SIGINT (CTRL+C) and exits the program
+    def handle_sigint(signum, x):
+        print("Interrupt detected, simulation shutting down...")
+        simulation_conn.send(END_MESSAGE)
+        render_process.send(END_MESSAGE)
+        simulation_process.join() # Dunno what that does...
+        render_process.join()
+        render_process.terminate() # Rendering needs to be killed explicitly
+        exit(0)
+
+    signal(SIGINT, handle_sigint)
+    while True: pass
+
 
 if __name__ == '__main__':
     _startup()
