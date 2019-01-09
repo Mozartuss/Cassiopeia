75eb5a23f1d6394f760def3a4018d0c15111d97b
Benjamin Altmiks <benjamin.altmiks@hs-augsburg.de>
2019-01-09 17:17:38 +0100

    fixing worker

--- cassiopeia/distribution/master.py
+++ cassiopeia/distribution/master.py
@@ -8,14 +8,15 @@ class Master():
         self.planets = planets
         self.delta_t = delta_t
         self.start_manager()
+        self.nr_of_workers = 8
     
     def create_argument_list(self):
         result_list = numpy.zeros((len(self.planets), 8))
-        index = -1
-        
+        index = -1        
 
-        for i in range(len(self.planets)):
-            self.job_queue.put((i, self.delta_t, self.planets))
+        step_size = len(self.planets)/self.nr_of_workers
+        for i in range(len(self.planets), steps=step_size):
+            self.job_queue.put((i, step_size, self.delta_t, self.planets))
         self.job_queue.join()
         
         while not self.result_queue.empty():
