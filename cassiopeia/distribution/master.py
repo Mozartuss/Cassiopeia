8a0ab2c583b63baf3ad06a4a4d8ebf5842e788cc
mo7art <lukas.kleybolte@hs-augsburg.de>
2019-01-09 19:11:44 +0100

    Man sieht die ersten Planeten und dan entsteht chaos

--- cassiopeia/distribution/master.py
+++ cassiopeia/distribution/master.py
@@ -2,36 +2,39 @@ from __future__ import print_function
 from distribution.taskManager import TaskManager
 import numpy
 
+
 class Master():
-    
+
     def __init__(self, planets, delta_t):
         self.planets = planets
         self.delta_t = delta_t
         self.start_manager()
-        self.nr_of_workers = 8
-    
+        self.nr_of_workers = 4
+
     def create_argument_list(self):
         result_list = numpy.zeros((len(self.planets), 8))
-        index = -1        
+        index = -1
 
-        step_size = int(len(self.planets)/self.nr_of_workers)
-        for i in range(len(self.planets), step_size):
+        step_size = int(len(self.planets) / self.nr_of_workers)
+
+        for i in range(0, len(self.planets), step_size):
             self.job_queue.put((i, step_size, self.delta_t, self.planets))
         self.job_queue.join()
-        
+
         while not self.result_queue.empty():
-            index = index + 1
-            result_list[index] = self.result_queue.get()
- 
+            for result in self.result_queue.get():
+                index = index + 1
+                result_list[index] = result
+            print("[create_argument_list] (result_list)", result_list)
+
         return result_list
-        
-    
+
     def start_manager(self):
-        server_ip = "127.0.0.1"
+        server_ip = "141.82.173.24"
         server_socket = 5003
         TaskManager.register('get_job_queue')
         TaskManager.register('get_result_queue')
-        manager = TaskManager(address=(server_ip, server_socket), authkey = b'secret')
-        manager.connect() #vor oder nachher
+        manager = TaskManager(address=(server_ip, server_socket), authkey=b'secret')
+        manager.connect()  # vor oder nachher
         self.result_queue = manager.get_result_queue()
         self.job_queue = manager.get_job_queue()
--- cassiopeia/distribution/taskManager.py
+++ cassiopeia/distribution/taskManager.py
@@ -3,21 +3,23 @@ from __future__ import print_function
 from multiprocessing.managers import BaseManager
 from multiprocessing import JoinableQueue, Queue
 
+
 class TaskManager(BaseManager):
     pass
-    
-#Hier muss man eigenltich nichts ändern für zwei Queues!
+
+
+# Hier muss man eigenltich nichts ändern für zwei Queues!
 
 if __name__ == '__main__':
     from sys import argv, exit
+
     master_socket = 5003
     task_queue = JoinableQueue()
     result_queue = Queue()
-    TaskManager.register('get_job_queue', 
-                         callable = lambda:task_queue)
-    TaskManager.register('get_result_queue', 
-                         callable = lambda:result_queue)
-    m = TaskManager(address = ('', master_socket), 
-                    authkey = b'secret')
+    TaskManager.register('get_job_queue',
+                         callable=lambda: task_queue)
+    TaskManager.register('get_result_queue',
+                         callable=lambda: result_queue)
+    m = TaskManager(address=('', master_socket), authkey=b'secret')
     print('starting queue server, socket', master_socket)
     m.get_server().serve_forever()
